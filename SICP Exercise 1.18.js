3096112995c1d4ca572d3cfd6789f9cbd95a69c2
Source Academy User <No public email provided>
2023-08-27 00:59:25 +0800

    Changes made from Source Academy

---
+++ SICP Exercise 1.18.js
@@ -0,0 +1,27 @@
+function is_even(n) {
+    return n % 2 === 0;
+}
+
+function double(a) {
+    return 2*a;
+}
+
+function halve(a) {
+    return a/2;
+}
+
+function fast_times(a, b) {
+    return a >= b
+        ? fast_times_iter(a, b, 0)
+        : fast_times_actual(b, a, 0);
+}
+
+function fast_times_iter(a, b, c) {
+    return b === 1
+        ? a
+        : is_even(b)
+        ? fast_times_actual(double(a), halve(b))
+        : a + fast_times_actual(a, b-1);
+}
+
+fast_times(3, 500);
