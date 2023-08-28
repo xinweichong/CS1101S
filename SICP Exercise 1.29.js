dad1c8331e896f151296f7ae587cee23d630ac79
Source Academy User <No public email provided>
2023-08-28 13:44:30 +0800

    Changes made from Source Academy

---
+++ SICP Exercise 1.29.js
@@ -0,0 +1,22 @@
+function sum(term, a, next, b) {
+    return a > b
+        ? 0
+        : term(a) + sum(term, next(a), next, b);
+}
+
+function cube(n) {
+    return n*n*n;
+}
+
+function integral(func, a, b, n) {
+    const h = (b-a)/(3*n);
+    function f(n) {
+        return a + (n * h);
+    }
+    function next(x) {
+        return x + (2*h);
+    }
+    return h*((func(a) + func(b)) + 4*sum(func, f(1), next, f(n-1)) + 2*sum(func, f(2), next, f(n-2)));
+}
+
+integral(cube, 0, 1, 1000);
