0f7783dd1ccb860c235722ae96d1e949eb6fb710
Source Academy User <No public email provided>
2023-08-28 16:12:22 +0800

    Changes made from Source Academy

--- SICP Exercise 1.29.js
+++ SICP Exercise 1.29.js
@@ -9,14 +9,14 @@ function cube(n) {
 }
 
 function integral(func, a, b, n) {
-    const h = (b-a)/(n);
+    const h = (b - a) / n;
     function f(n) {
-        return a + (n * (h));
+        return a + (n * h);
     }
     function next(x) {
-        return x + (2 * (h));
+        return x + (2 * h;
     }
-    return (h/3)*((func(a) + func(b)) + 4*sum(func, f(1), next, f(n-1)) + 2*sum(func, f(2), next, f(n-2)));
+    return (h / 3)*((func(a) + func(b)) + 4 * sum(func, f(1), next, f(n - 1)) + 2 * sum(func, f(2), next, f(n - 2)));
 }
 
 integral(cube, 0, 1, 1000);
