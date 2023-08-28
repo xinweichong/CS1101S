eb5045145acd97f64e051e81df74597afacdc78e
Source Academy User <No public email provided>
2023-08-28 16:33:21 +0800

    Changes made from Source Academy

--- SICP Exercise 1.29.js
+++ SICP Exercise 1.29.js
@@ -9,13 +9,17 @@ function cube(n) {
 }
 
 function integral(func, a, b, n) {
+    // define h
     const h = (b - a) / n;
+    // define function to return yk
     function f(n) {
         return a + (n * h);
     }
+    // define function to cycle to y(k+2)
     function next(x) {
         return x + (2 * h);
     }
+    // Simpson's rule
     return (h / 3) * (
                       (func(a) + func(b)) 
                        + 4 * sum(func, f(1), next, f(n - 1)) 
