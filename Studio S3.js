4eed81763aadfe2682cc7b0fc497f69a85164bf0
Source Academy User <No public email provided>
2023-08-28 16:12:28 +0800

    Changes made from Source Academy

--- Studio S3.js
+++ Studio S3.js
@@ -3,13 +3,13 @@ import {beside, stack, blank, square, heart, show} from "rune";
 function f1(rune_1, n, rune_2) {
     return n === 0
         ? rune_2
-        : f1(rune_1, n-1, beside(rune_1, stack(blank, rune_2)));
+        : f1(rune_1, n - 1, beside(rune_1, stack(blank, rune_2)));
 }
 
 function f2(rune, n) {
-    return n ===0
+    return n === 0
         ? rune
-        : stack(beside(blank, f2(rune, n-1)), square);
+        : stack(beside(blank, f2(rune, n - 1)), square);
 }
 
 show(f1(square, 3, heart));
