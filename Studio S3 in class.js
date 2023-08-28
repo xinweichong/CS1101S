731863b6cf1c126b4bdf0f26a14188d8145b0178
Source Academy User <No public email provided>
2023-08-28 17:13:11 +0800

    Changes made from Source Academy

--- Studio S3 in class.js
+++ Studio S3 in class.js
@@ -1,21 +1,32 @@
 import {beside, stack, stack_frac, beside_frac, blank, square, heart, ribbon, circle, show} from "rune";
 
-function moony_1(bottom_right, n) {
-    return stack_frac((n - 1) / n,
-                      beside_frac((n - 1) / n, circle, blank),
-                      beside_frac((n - 1) / n, square, bottom_right));
+function moony_1(bottom_right) {
+    return stack(beside_frac(circle, blank),
+                 beside_frac(square, bottom_right));
 }
 
-/*function moony_2(n) {
+function moony_3(bottom_right, n) {
+    return stack_frac(1 / n,
+                      beside_frac(1 / n, circle, blank),
+                      beside_frac(1 / n, square, bottom_right));
+}
+
+function moony_2(n) {
     return n === 1
         ? circle
         : moony_1(moony_2(n-1));
-}*/
+}
 
 function moony(n) {
     return n === 1
         ? circle
-        : moony_1(moony(n-1), n);
+        : moony_3(moony(n-1), n);
+}
+
+function moony_4(n) {
+    return n === 1
+        ? circle
+        : moony_1(scale(moony_4(n-1)));
 }
 
 show(moony_2(5));
