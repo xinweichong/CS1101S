a4acbe73b303ce9ec7820c01a4adf106e724a034
Source Academy User <No public email provided>
2023-08-29 11:35:50 +0800

    Changes made from Source Academy

---
+++ SICP/Exercise 1.33.js
@@ -0,0 +1,70 @@
+function sum(term, a, next, b) {
+    return a > b
+           ? 0
+           : term(a) + sum(term, next(a), next, b);
+}
+
+function product(term, a, next, b) {
+    return a > b
+           ? 1
+           : term(a) * product(term, next(a), next, b);
+}
+
+function sum_iter(term, a, next, b) {
+    function iter(a, result) {
+        return a > b
+               ? result
+               : iter(next(a), term(a) + result);
+    }
+    return iter(a, 0);
+}
+
+function product_iter(term, a, next, b) {
+    function iter(a, result) {
+        return a > b
+               ? result
+               : iter(next(a), term(a) * result);
+    }
+    return iter(a, 1);
+}
+
+function inc(n) {
+    return n + 1;
+}
+
+function accumulate(combiner, null_value, term, a, next, b) {
+    return a > b
+           ? null_value
+           : combiner(term(a), accumulate(combiner, null_value, term, next(a), next, b)); 
+}
+
+function accumulate_iter(combiner, null_value, term, a, next, b) {
+    function iter(a, result) {
+        return a > b
+               ? result
+               : iter(next(a), combiner(term(a), result));
+    }
+    return iter(a, null_value);
+}
+
+function sum_2(a, b) {
+    return a + b;
+}
+
+function product_2(a, b) {
+    return a * b;
+}
+
+function cube(a) {
+    return a * a * a;
+}
+
+function filtered_accumulate(combiner, null_value, filter, term, a, next, b) {
+    return a > b
+           ? null_value
+           : filter(a) 
+             ? combiner(term(a), filtered_accumulate(combiner, null_value, term, next(a), next, b))
+             : filtered_accumulate(combiner, null_value, term, next(a), next, b);
+}
+
+accumulate_iter(sum_2, 0, cube, 1, inc, 4);
