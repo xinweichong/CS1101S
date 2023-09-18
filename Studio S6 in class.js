bcc4e463e01cebe4d86f648a7cc095523e2ccdb2
Source Academy User <No public email provided>
2023-09-18 16:50:03 +0800

    Changes made from Source Academy

--- Studio S6 in class.js
+++ Studio S6 in class.js
@@ -1,6 +1,18 @@
 function remove_duplicates(lst) {
-    return accumulate( (head,tail) => pair(head, filter(z => !equal(z, head), tail)), null, lst);
+    return accumulate( (head,tail) => pair(head, 
+                                           filter(z => !equal(z, head), tail)), 
+                      null, 
+                      lst);
 }
 
-display_list(remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1)));
+//display_list(remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1)));
 
+function subsets(xs) {
+    if (xs === null) {
+        return list(null);
+    } else {
+        const use_head = map(x => pair())subsets(tail(xs));
+        
+        const 
+    }
+}
