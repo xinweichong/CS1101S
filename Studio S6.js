070125950338a8d4108b4720350fab46f1f87467
Source Academy User <No public email provided>
2023-09-18 16:36:50 +0800

    Changes made from Source Academy

--- Studio S6.js
+++ Studio S6.js
@@ -21,11 +21,13 @@ function makeupamount(x, coins) {
     } else if (x < 0 || is_null(coins)) {
         return null;
     } else {
+        //Combinations that don't use the head coin
         const combi_A = makeupamount(x, tail(coins));
-        
+        //Combinations that don't use head coin for remaining amount
         const combi_B = makeupamount(x - head(coins), tail(coins));
+        // Combinations that use the head coin (adding the head coin back to the front of those that already used it at the start)
+        const combi_C = map(x => pair(head(coins), x), combi_B);
         
-        const combi_C = map();
         
         return append(combi_A, combi_C);
     }
