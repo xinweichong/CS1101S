81053357d7fb7ec8e7eea5a93981af36b332ee0e
Source Academy User <No public email provided>
2023-09-18 17:25:04 +0800

    Changes made from Source Academy

--- Studio S6 in class.js
+++ Studio S6 in class.js
@@ -5,14 +5,45 @@ function remove_duplicates(lst) {
                       lst);
 }
 
-//display_list(remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1)));
+function remove_duplicates_2(lst) {
+    return accumulate( ((head, tail) => is_null(member(head, tail))
+                                            ? pair(head, tail)
+                                            : tail), 
+                      null, 
+                      lst);
+}
+
+//display_list(remove_duplicates_2(list(1, 2, 3, 4, 4, 3, 2, 1)));
 
 function subsets(xs) {
-    if (xs === null) {
+    if (is_null(xs)) {
         return list(null);
     } else {
-        const use_head = map(x => pair())subsets(tail(xs));
         
-        const 
+        const without_head = subsets(tail(xs));
+        
+        const use_head = map(x => pair(head(xs), x), subsets(tail(xs)));
+        
+        return append(use_head, without_head);
     }
 }
+
+//display_list(subsets(list(1, 2, 3)));
+
+
+function permutations(xs) {
+    if (is_null(xs)) {
+        return list(null);
+    } else {
+        
+        const map_heads = map(x => pair(x, permutations(filter(y => !equal(x, y), xs))), xs);
+        
+        //const without_head = permutations(map_heads);
+        
+        //const use_head = map(x => pair(head(xs), x), without_head);
+        
+        return map_heads;
+    }
+}
+
+display_list(permutations(list(1, 2, 3)));
