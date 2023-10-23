b706bd790574eceeb0f6dd935decd73558f0cd19
Source Academy User <No public email provided>
2023-10-23 17:12:10 +0800

    Changes made from Source Academy

--- Studio S10 in class.js
+++ Studio S10 in class.js
@@ -1,14 +1,30 @@
 function transpose(M) {
-    const og_rows = array_length(M);
-    const og_cols = array_length(M[0]);
-    const new_M = [];
+    const width = array_length(M);
     
-    for (let r = 0; r < og_cols; r = r + 1) {
-        new_M[r] = [];
-        for (let c = 0; c < og_rows; c = c + 1) {
-            new_M[r][c] = M[c][r];
+    const half_width = math_floor(width / 2);
+    
+    for (let i = 0; i < width; i = i + 1) {
+        for (let j = i + 1; j < width; j = j + 1) {
+            const temp = M[i][j];
+            M[i][j] = M[j][i];
+            M[j][i] = temp;
         }
     }
     
-    return new_M;
+    for (let k = 0; k < width; k = k + 1) {
+        for (let l = 0; l < half_width; l = l + 1) {
+            const temp2 = M[k][l];
+            M[k][l] = M[k][width - l - 1];
+            M[k][width - l - 1] = temp2;
+        }
+        
+    }
+    
+    return M;
 }
+
+let a = [[1, 2, 3, 4], 
+         [5, 6, 7, 8], 
+         [9, 10, 11, 12],
+         [13, 14, 15, 16]];
+transpose(a);
