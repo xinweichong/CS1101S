6d6e350c292ce5abb1ab6cae770163cb1d9abf1e
Source Academy User <No public email provided>
2023-11-03 12:54:25 +0800

    Changes made from Source Academy

--- PA/2223 Q3.js
+++ PA/2223 Q3.js
@@ -3,24 +3,35 @@
 
 function make_NiFT(T) {
     
+    // if (is_number(T)) {
+    //     return T;
+    // } else {
+    //     let nums = null;
+    //     let trees = null;
+        
+    //     const len = length(T);
+        
+    //     for (let i = T; !is_null(i); i = tail(i)) {
+    //         if (is_number(head(i))) {
+    //             nums = pair(head(i), nums);
+    //         } else {
+    //             trees = pair(head(i), trees);
+    //         }
+    //     }
+        
+    //     nums = reverse(nums);
+    //     trees = reverse(trees);
+        
+    //     trees = map(x => make_NiFT(x), trees);
+        
+    //     return append(nums, trees);
+    // }
+    
     if (is_number(T)) {
         return T;
     } else {
-        let nums = null;
-        let trees = null;
-        
-        const len = length(T);
-        
-        for (let i = T; !is_null(i); i = tail(i)) {
-            if (is_number(head(i))) {
-                nums = pair(head(i), nums);
-            } else {
-                trees = pair(head(i), trees);
-            }
-        }
-        
-        nums = reverse(nums);
-        trees = reverse(trees);
+        const nums = filter(x => is_number(x), T);
+        let trees = filter(x => !is_number(x), T);
         
         trees = map(x => make_NiFT(x), trees);
         
@@ -32,16 +43,16 @@ function make_NiFT(T) {
 
 function insert(x, xs) {
     return is_null(xs) 
-           ? list(x)
-           : x <= head(xs)
-           ? pair(x, xs)
-           : pair(head(xs, insert(x, tail(xs))));
+          ? list(x)
+          : x <= head(xs)
+          ? pair(x, xs)
+          : pair(head(xs), insert(x, tail(xs)));
 }
 
 function insertion_sort(xs) {
     return is_null(xs)
-           ? xs
-           : insert(head(xs), insertion_sort(tail(xs)));
+          ? xs
+          : insert(head(xs), insertion_sort(tail(xs)));
 }
 
 function map_tree(fun, tree) {
@@ -52,6 +63,24 @@ function map_tree(fun, tree) {
               tree);
 }
 
+function flatten_tree(tree) {
+    return is_null(tree)
+            ? null
+            : !is_list(head(tree))
+            ? pair(head(tree), flatten_tree(tail(tree)))
+            : append(flatten_tree(head(tree)), 
+                     flatten_tree(tail(tree)));
+}
+
 function make_SToN(T) {
+    let ls = flatten_tree(T);
+    ls = insertion_sort(ls);
     
+    function traverse(x) {
+        const hd = head(ls);
+        ls = tail(ls);
+        return hd;
+    }
+    
+    return map_tree(traverse, T);
 }
