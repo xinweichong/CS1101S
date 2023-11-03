c93beb3beb28ab1597a3ab531f3261da98325574
Source Academy User <No public email provided>
2023-11-03 10:22:13 +0800

    Changes made from Source Academy

---
+++ PA/2223 Q1.js
@@ -0,0 +1,12 @@
+function insert_subseq(L, pos, S) {
+    
+    let result = null;
+    
+    for (let i = 0; i < pos; i = i + 1) {
+        result = pair(head(L));
+        L = tail(L);
+    }
+    
+    
+    
+}
