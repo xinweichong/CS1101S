dfece25cf77da3bd74251b7853ea119e29b777ae
Source Academy User <No public email provided>
2023-11-03 10:51:27 +0800

    Changes made from Source Academy

--- PA/2223 Q1.js
+++ PA/2223 Q1.js
@@ -2,22 +2,19 @@
 function insert_subseq(L, pos, S) {
     
     let result = null;
+    const len_L = length(L);
+    const len_S = length(S);
     
-    for (let i = 0; i < pos; i = i + 1) {
-        result = pair(head(L), result);
-        L = tail(L);
+    for (let i = 0; i < (len_L + len_S); i = i + 1) {
+        if (i >= pos && !is_null(S)) {
+            result = pair(head(S), result);
+            S = tail(S);
+        } else {
+            result = pair(head(L), result);
+            L = tail(L);
+        }
     }
-    
-    for (let j = S; !is_null(j); j = tail(j)) {
-        result = pair(head(j), result);
-    }
-    
-    for (let k = L; !is_null(k); k = tail(k)) {
-        result = pair(head(k), result);
-    }
-    
     return reverse(result);
-    
 }
 
 
@@ -25,24 +22,17 @@ function insert_subseq(L, pos, S) {
 function remove_subseq(L, start_pos, end_pos) {
     
     let result = null;
-    // let count = 0;
+    const len = length(L);
     
-    // for (let i = L; !is_null(i); i = tail(i)) {
-    //     if ((count < start_pos) || (count > end_pos)) {
-    //         result = pair(head(i), result);
-    //     }
-    //     count = count + 1;
-    // }
-    
-    for (let i = 0; i < length(L); i = i + 1) {
+    for (let i = 0; i < len; i = i + 1) {
         if ((i < start_pos) || (i > end_pos)) {
             result = pair(head(L), result);
         }
         L = tail(L);
     }
     
-    return (result);
+    return reverse(result);
     
 }
 
-remove_subseq( list(10, 11, 12, 13, 14, 15, 16), 2, 4 );
+//
